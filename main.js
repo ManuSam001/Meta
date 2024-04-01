d0ba9a3eafbd3d8120a0e7e9059c300f4c5edb0c
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2024-04-01 10:34:30 -0300

    .

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ for(let i=0; i <botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempo0objetivo1 = new Date("2024-12-21T00:00:00");
 let tempoAtual = new Date();
+
 contadores[0].textContent = calculaTempo(tempo0objetivo1);
 
 function calculaTempo(tempo0objetivo){
@@ -27,7 +28,7 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = dias + " dias " + horas + " horas " + 
+return dias + " dias " + horas + " horas " + 
 minutos + " minutos " + segundos + " sugundos ";
 }
     
