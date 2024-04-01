0d469c1e964743f2733313d98d9fa08809ed4109
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2024-04-01 10:28:15 -0300

    .

--- main.js
+++ main.js
@@ -14,17 +14,21 @@ for(let i=0; i <botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempo0objetivo1 = new Date("2024-12-21T00:00:00");
 let tempoAtual = new Date();
-contadores[0].textContent = calculaTempo(tempo0objetivo1);
+//contadores[0].textContent = calculaTempo(tempo0objetivo1);
 
-function calculaTempo(tempo0objetivo1){
+//function calculaTempo(tempo0objetivo1){
 
-}
+//}
     
 let tempoFinal = tempo0objetivo1 - tempoAtual;
 let segundos = Math.floor(tempoFinal/1000); -
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
 
 contadores[0].textContent = dias + "dias" + horas + "horas" + 
 minutos + "minutos" + segundos + "sugundos";
