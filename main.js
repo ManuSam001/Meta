be457922af337aa208dc597657d3923cd48679d9
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2024-04-05 11:26:04 -0300

    .

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ for(let i=0; i <botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-09-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-31T00:00:00");
+const tempoObjetivo2 = new Date("2024-09-27T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-31T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
