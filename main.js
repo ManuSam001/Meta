61aa25ee1bb0997e793538a6a6d8b30a210258f5
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2024-04-08 10:33:57 -0300

    .

--- main.js
+++ main.js
@@ -20,17 +20,20 @@ const tempoObjetivo4 = new Date("2024-06-31T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date();
-   let tempoFinal = tempoObjetivo - tempoAtual;
-   let segundos = Math.floor(tempoFinal/1000);
-   let minutos = Math.floor(segundos/60);
-   let horas = Math.floor(minutos/60);
-   let dias = Math.floor(horas/24);
-
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
+
+if(tempoFinal > 0){
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+} else {
+    return "Prazo Finalizado";
 }
     
 function atualizaCronometro(){
